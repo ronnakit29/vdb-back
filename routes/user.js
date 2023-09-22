e5bce41e211bdf7ce3dd41b55d1c37f2b51cbc9f
ronnakit29 <miku.fc.net@gmail.com>
2023-09-22 09:35:09 +0700

    tel create

--- routes/user.js
+++ routes/user.js
@@ -55,7 +55,7 @@ router.get('/list', acceptRole(["master", "manager"]), async (req, res) => {
 router.post('/create', async (req, res) => {
 	try {
 		const currentUser = req.user;
-		const { role, username, fullname, password, email, citizen_id, village_code } = req.body;
+		const { role, username, fullname, password, email, citizen_id, village_code, tel } = req.body;
 		if (!village_code) throw new Error('village_code is required')
 		if (!citizen_id) throw new Error('citizen_id is required')
 		const managerRoleAccept = ["manager", "employee"]
@@ -65,11 +65,12 @@ router.post('/create', async (req, res) => {
 			email,
 			citizen_id,
 			fullname,
+			tel: tel || "",
 			village_code: currentUser.role === 'master' ? village_code : currentUser.village_code,
 			role: currentUser === "master" ? role : managerRoleAccept.includes(currentUser.role) ? role : "employee",
 		}
 		const authen = new Authen(user);
-		const result = await authen.register(params.username, params.password, params.fullname, params.email, params.role, params.citizen_id, params.tel, params.village_code);
+		const result = await authen.register(params.username, params.password, params.fullname, params.email, params.role, params.citizen_id, params.tel, params.village_code, tel);
 		return res.status(200).json({ success: true, data: result });
 	} catch (error) {
 		console.error(error);
