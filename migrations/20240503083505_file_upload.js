819814ebd09c3a3113d078f2d988288810d50119
ronnakit290 <miku.fc.net@gmail.com>
2024-05-03 15:37:50 +0700

    file

---
+++ migrations/20240503083505_file_upload.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('income_expenses', function (table) {
+    table.string('file')
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
