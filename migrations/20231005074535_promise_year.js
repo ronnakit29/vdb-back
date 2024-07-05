68e231af521ca6bad3b6e4f365ae4a253344945d
ronnakit290 <miku.fc.net@gmail.com>
2024-07-05 16:10:28 +0700

    chore: Update promise_year migration to remove "createTableIfNotExists" method

--- migrations/20231005074535_promise_year.js
+++ migrations/20231005074535_promise_year.js
@@ -3,22 +3,28 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-	return knex.schema.createTable('promise_year', table => {
-		table.increments('id').primary();
-		table.string('year').defaultTo('2566')
-		table.integer('village_id')
-	}).then(() => {
-		return knex("village").select("id").then(villages => {
-			const villageIds = villages.map(village => village.id)
-			const promiseYears = villageIds.map(villageId => {
-				return {
-					year: "2566",
-					village_id: villageId
-				}
+	knex.schema.hasTable('promise_year').then(function (exists) {
+		if (!exists) {
+
+			return knex.schema.createTable('promise_year', table => {
+				table.increments('id').primary();
+				table.string('year').defaultTo('2566')
+				table.integer('village_id')
+			}).then(() => {
+				return knex("village").select("id").then(villages => {
+					const villageIds = villages.map(village => village.id)
+					const promiseYears = villageIds.map(villageId => {
+						return {
+							year: "2566",
+							village_id: villageId
+						}
+					})
+					return knex("promise_year").insert(promiseYears)
+				})
 			})
-			return knex("promise_year").insert(promiseYears)
-		})
-	})
+
+		}
+	});
 };
 
 /**
